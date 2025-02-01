3badd287a13b3b925e566a2e4ca35ae37a31edfa
Sakamochanq <Sakamochan.q@outlook.jp>
2025-02-01 20:57:38 +0900

    feat: ハンバーガーメニューの実装

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 
 export const Header = () => {
+  const [MenuOpen, setMenuOpen] = useState(false)
+  const toggleMenu = () => {
+    setMenuOpen(!MenuOpen)
+  }
+  const closeMenu = (e) => {
+    if (!e.target.closest('.menu-item') && !e.target.closest('.menu-button')) {
+      setMenuOpen(false)
+    }
+  }
+  React.useEffect(() => {
+    document.addEventListener('click', closeMenu)
+    return () => {
+      document.removeEventListener('click', closeMenu)
+    }
+  }, [])
+
   return (
     <header>
         <h1 className='nav-logo'>Logo</h1>
-        <button className="menu-button">
+        <button onClick={toggleMenu} className="menu-button">
           <span></span><span></span><span></span>
         </button>
-        <nav>
+        <nav className={`menu-modal ${MenuOpen ? 'open' : ''}`}>
             <ul className='menu'>
                 <li className='menu-item'><a href="#">Home</a></li>
                 <li className='menu-item'><a href="#">About</a></li>
