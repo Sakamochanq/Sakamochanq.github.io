efba26abc5f2fd29847800ef15844ae14a640873
Sakamochanq <Sakamochan.q@outlook.jp>
2025-02-01 22:44:12 +0900

    feat: Homeの作成

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+export const Home = () => {
+  return (
+    <div className="home-container">
+        <img className='logo' src={process.env.PUBLIC_URL + './assets/images/Deer.png'}/>
+        <br /><br />
+        <h2>Hello World.</h2>
+        <br /><br />
+        <hr style={{width: "65%"}}/>
+    </div>
+  )
+}
+export default Home
